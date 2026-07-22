03d08fd802cb35ff6370822e35f8c63f671424a2
Mr Hamed <b24.hamed@gmail.com>
2026-07-22 19:44:00 +0330

    feat: add entropy and information gain calculations

--- models/decision_tree.py
+++ models/decision_tree.py
@@ -17,3 +17,23 @@ class DecisionTree:
         self.min_samples_split = min_samples_split
         self.max_depth = max_depth
         self.root = None
+
+    def _entropy(self, y):
+        hist = np.bincount(y.astype(np.int64))
+        ps = hist / len(y)
+        return -np.sum([p * np.log2(p) for p in ps if p > 0])
+
+    def _split(self, X_column, split_thresh):
+        left_idxs = np.argwhere(X_column <= split_thresh).flatten()
+        right_idxs = np.argwhere(X_column > split_thresh).flatten()
+        return left_idxs, right_idxs
+
+    def _information_gain(self, y, X_column, threshold):
+        parent_entropy = self._entropy(y)
+        left_idxs, right_idxs = self._split(X_column, threshold)
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+        n = len(y)
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        e_l, e_r = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
+        return parent_entropy - ((n_l / n) * e_l + (n_r / n) * e_r)
