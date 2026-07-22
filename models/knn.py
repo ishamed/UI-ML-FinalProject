620d908827b60c2040e6b008ede52c38b99ef910
Mr Hamed <b24.hamed@gmail.com>
2026-07-22 19:15:49 +0330

    feat: implement single sample prediction

--- models/knn.py
+++ models/knn.py
@@ -8,3 +8,15 @@ class KNN:
     def fit(self, X, y):
         self.X_train = np.array(X)
         self.y_train = np.array(y)
+
+    def _predict_single(self, x):
+        distances = [np.linalg.norm(x - x_train) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        most_common = Counter(k_nearest_labels).most_common(1)
+        return most_common[0][0]
+
+    def predict(self, X):
+        X = np.array(X)
+        return np.array([self._predict_single(x) for x in X])
+    
