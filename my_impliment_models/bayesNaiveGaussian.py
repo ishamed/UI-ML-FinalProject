a2dd4f9db24801807b7fa25791a7772568986b6e
amir <amir5544332211aaaa@gmail.com>
2026-07-22 21:07:57 +0330

    feat(naive_bayes): implement fit and predict methods using Bayes theorem

--- my_impliment_models/bayesNaiveGaussian.py
+++ my_impliment_models/bayesNaiveGaussian.py
@@ -13,3 +13,31 @@ class GaussianNaiveBayesFromScratch:
         numerator = np.exp(-((x - mean) ** 2) / (2 * var))
         denominator = np.sqrt(2 * np.pi * var)
         return numerator / denominator
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.classes = np.unique(y)
+        n_classes = len(self.classes)
+
+        self.mean = np.zeros((n_classes, n_features))
+        self.var = np.zeros((n_classes, n_features))
+        self.priors = np.zeros(n_classes)
+
+        for idx, c in enumerate(self.classes):
+            X_c = X[y == c]
+            self.mean[idx, :] = X_c.mean(axis=0)
+            self.var[idx, :] = X_c.var(axis=0) + 1e-9
+            self.priors[idx] = X_c.shape[0] / float(n_samples)
+
+    def predict(self, X):
+        return np.array([self._predict_row(x) for x in X])
+
+    def _predict_row(self, x):
+        posteriors = []
+
+        for idx, c in enumerate(self.classes):
+            prior = np.log(self.priors[idx])
+            posterior = np.sum(np.log(self._pdf(idx, x) + 1e-9))
+            posteriors.append(prior + posterior)
+
+        return self.classes[np.argmax(posteriors)]
