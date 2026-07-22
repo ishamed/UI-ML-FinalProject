f655e95efd574050b5c217c3f34a67b7ffa77cf4
amir <amir5544332211aaaa@gmail.com>
2026-07-22 19:59:47 +0330

    feat(adaboost): implement fit method with sample re-weighting logic

--- my_impliment_models/adaBoost.py
+++ my_impliment_models/adaBoost.py
@@ -23,3 +23,48 @@ class AdaBoost:
     def __init__(self, n_estimators=50):
         self.n_estimators = n_estimators
         self.clfs = []
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+
+        y_signed = np.where(y <= 0, -1, 1)
+
+        w = np.full(n_samples, (1 / n_samples))
+
+        self.clfs = []
+
+        for _ in range(self.n_estimators):
+            clf = DecisionStump()
+            min_error = float('inf')
+
+            for feature_i in range(n_features):
+                X_column = X[:, feature_i]
+                thresholds = np.unique(X_column)
+
+                for threshold in thresholds:
+                    for polarity in [1, -1]:
+                        predictions = np.ones(n_samples)
+                        if polarity == 1:
+                            predictions[X_column < threshold] = -1
+                        else:
+                            predictions[X_column > threshold] = -1
+
+                        error = sum(w[y_signed != predictions])
+
+                        if error < min_error:
+                            min_error = error
+                            clf.polarity = polarity
+                            clf.threshold = threshold
+                            clf.feature_idx = feature_i
+
+            EPS = 1e-10
+            min_error = np.clip(min_error, EPS, 1 - EPS)
+
+            clf.alpha = 0.5 * np.log((1.0 - min_error) / min_error)
+
+            predictions = clf.predict(X)
+            w *= np.exp(-clf.alpha * y_signed * predictions)
+
+            w /= np.sum(w)
+
+            self.clfs.append(clf)
