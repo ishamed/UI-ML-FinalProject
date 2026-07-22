a7055a0d6651b61a2ba10f76ff59decada411751
Mr Hamed <b24.hamed@gmail.com>
2026-07-22 19:00:04 +0330

    feat: implement gradient descent in fit method

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -9,3 +9,20 @@ class LogisticRegression:
 
     def _sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
+
+    def fit(self, X, y):
+        X = np.array(X)
+        y = np.array(y)
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.epochs):
+            linear_model = np.dot(X, self.weights) + self.bias
+            y_predicted = self._sigmoid(linear_model)
+
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
