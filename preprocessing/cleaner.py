a7e6f0c4bd4932e2bd12d0aee722f83bd039cba9
Mr Hamed <b24.hamed@gmail.com>
2026-07-22 17:00:08 +0330

    feat: add histogram and boxplot to EDA

--- preprocessing/cleaner.py
+++ preprocessing/cleaner.py
@@ -6,6 +6,16 @@ from sklearn.impute import KNNImputer
 import seaborn as sns
 
 def perform_eda(df):
+    df.hist(figsize=(15, 12), bins=30, edgecolor='black', color='skyblue')
+    plt.suptitle("Histogram", fontsize=16)
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.show()
+
+    plt.figure(figsize=(15, 10))
+    sns.boxplot(data=df.drop("Potability", axis=1), orient="h", palette="Set2")
+    plt.title("Boxplot", fontsize=16)
+    plt.show()
+
     plt.figure(figsize=(10,10))
     sns.heatmap(df.corr(), annot=True, cmap="coolwarm", fmt=".2f")
     plt.title("Correlation Matrix")
