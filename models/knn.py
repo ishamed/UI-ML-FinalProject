ee31d9056bb5b116d8485e4fc554b1227c627b37
Mr Hamed <b24.hamed@gmail.com>
2026-07-22 19:11:13 +0330

    feat: setup class and implement fit method

---
+++ models/knn.py
@@ -0,0 +1,10 @@
+import numpy as np
+from collections import Counter
+
+class KNN:
+    def __init__(self, k=5):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = np.array(X)
+        self.y_train = np.array(y)
