bdbd03e5ec45f97a3bf0375ab21f10af53884b90
Mr Hamed <b24.hamed@gmail.com>
2026-07-22 19:30:05 +0330

    feat: implement gradient descent with hinge loss in fit method and implement predict method with binary thresholding

--- models/svm.py
+++ models/svm.py
@@ -7,3 +7,26 @@ class SVM:
         self.epochs = epochs
         self.weights = None
         self.bias = None
+
+    def fit(self, X, y):
+        X = np.array(X)
+        y = np.array(y)
+        n_samples, n_features = X.shape
+        y_ = np.where(y <= 0, -1, 1)
+
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.epochs):
+            for idx, x_i in enumerate(X):
+                condition = y_[idx] * (np.dot(x_i, self.weights) - self.bias) >= 1
+                if condition:
+                    self.weights -= self.lr * (2 * self.lambda_param * self.weights)
+                else:
+                    self.weights -= self.lr * (2 * self.lambda_param * self.weights - np.dot(x_i, y_[idx]))
+                    self.bias -= self.lr * y_[idx]
+
+    def predict(self, X):
+        X = np.array(X)
+        approx = np.dot(X, self.weights) - self.bias
+        return np.where(approx >= 0, 1, 0)
