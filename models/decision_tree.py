702915f03b65d14bc71c1517ae730c6e059bed00
Mr Hamed <b24.hamed@gmail.com>
2026-07-22 19:34:16 +0330

    create node structure and tree base class

---
+++ models/decision_tree.py
@@ -0,0 +1,19 @@
+import numpy as np
+from collections import Counter
+
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+class DecisionTree:
+    def __init__(self, min_samples_split=2, max_depth=100):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.root = None
