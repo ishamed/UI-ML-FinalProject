2a836ee4ffa48b56e4e4ab1566222afe4fdb2304
Mr Hamed <b24.hamed@gmail.com>
2026-07-22 19:24:20 +0330

    feat: initialze hyperparameters for linear svm

---
+++ models/svm.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class SVM:
+    def __init__(self, learning_rate=0.001, lambda_param=0.01, epochs=1000):
+        self.lr = learning_rate
+        self.lambda_param = lambda_param
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
